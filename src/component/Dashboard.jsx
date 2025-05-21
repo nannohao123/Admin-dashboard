8d9fbdaa654903ef348924fef25f9001d2c30395
Agung Nabhan <agungnabhan414@gmail.com>
2025-05-21 21:19:14 +0700

    update tampilan dashboard

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -8,10 +8,10 @@ import { Calendar } from 'lucide-react';
 
 function Dashboard() {
   const contentItems = [
-    { tittle: 'Santri', color: '#FAC0C6', icon: <User className='static-icon' />, dCount: '5,000', fontColor: '#83575C', bColor:'#FA5A7D'},
-    { tittle: 'Guru', color: '#F5D99F', icon: <Presentation className='static-icon' />, dCount: '1,500', fontColor: '#6E634D', bColor:'#FF947A' },
-    { tittle: 'Pegawai', color: '#B6F5CB', icon: <Building className='static-icon' />, dCount: '6,000', fontColor: '#52765E', bColor:'#3CD856' },
-    { tittle: 'Alumni', color: '#DDBEFF', icon: <GraduationCap className='static-icon' />, dCount: '200,000', fontColor: '#6B5D79', bColor:'#BF83FF' }
+    { tittle: 'Santri', color: '#FAC0C6', icon: <User className='static-icon' />, dCount: '5,000', fontColor: '#83575C', bColor: '#FA5A7D' },
+    { tittle: 'Guru', color: '#F5D99F', icon: <Presentation className='static-icon' />, dCount: '1,500', fontColor: '#6E634D', bColor: '#FF947A' },
+    { tittle: 'Pegawai', color: '#B6F5CB', icon: <Building className='static-icon' />, dCount: '6,000', fontColor: '#52765E', bColor: '#3CD856' },
+    { tittle: 'Alumni', color: '#DDBEFF', icon: <GraduationCap className='static-icon' />, dCount: '200,000', fontColor: '#6B5D79', bColor: '#BF83FF' }
   ]
 
   const upcomingEvents = [
@@ -43,7 +43,7 @@ function Dashboard() {
             <div className='d-data' style={{
               marginLeft: '45px',
               marginTop: '30px',
-              height: '300px',
+              height: '350px',
               backgroundColor: 'white',
               color: 'black',
               padding: '10px',
@@ -66,24 +66,23 @@ function Dashboard() {
               <h1>Calender</h1>
             </div>
             <div className='event' style={{
-              border:'1px solid black',
-              width:'460px',
-              height:'200px',
+              width: '460px',
+              height: '200px',
               marginLeft: '40px',
-              marginTop:'28px'
-              }}>
-                <div className='top-event' style={{ display:'flex', justifyContent:'space-between', alignItems:'center', padding:'10px', marginTop:'-30px', color:'black'}}>
-                  <h2>Upcoming Event</h2>
-                  <p style={{ textDecoration:'underline', cursor:'pointer' }}>See all</p>
-                </div>
-                {upcomingEvents.map((item, index) => (
-                  <EventDetails
+              marginTop: '28px'
+            }}>
+              <div className='top-event' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px', marginTop: '-30px', color: 'black', marginBottom: '-20px' }}>
+                <h2>Upcoming Event</h2>
+                <p style={{ textDecoration: 'underline', cursor: 'pointer' }}>See all</p>
+              </div>
+              {upcomingEvents.map((item, index) => (
+                <EventDetails
                   key={index}
                   icon={item.icon}
                   eName={item.eName}
                   dName={item.dName}
-                  />
-                ))}
+                />
+              ))}
             </div>
           </div>
         </div>
@@ -108,26 +107,50 @@ function ContentCard({ tittle, color, icon, dCount, fontColor, bColor }) {
         padding: '10px',
         width: '30px',
         height: '30px',
-        paddingLeft:'17px',
-        paddingTop:'17px',
-        marginTop:'5px',
-        marginLeft:'10px',
-        backgroundColor:bColor
+        paddingLeft: '17px',
+        paddingTop: '17px',
+        marginTop: '5px',
+        marginLeft: '10px',
+        backgroundColor: bColor
       }}>
         {icon}
       </div>
-      <h2 style={{ color: fontColor, marginLeft:'10px', }}>{dCount}</h2>
-      <h3 style={{ color: fontColor, fontWeight: 'normal', marginTop:'-15px',fontSize:'15px', marginLeft:'10px',}}>{tittle}</h3>
+      <h2 style={{ color: fontColor, marginLeft: '10px', }}>{dCount}</h2>
+      <h3 style={{ color: fontColor, fontWeight: 'normal', marginTop: '-15px', fontSize: '15px', marginLeft: '10px', }}>{tittle}</h3>
     </div>
   )
 }
 
-function EventDetails({icon, eName, dName}){
-  return(
-    <div className='eventdetails' style={{ border:'1px solid black' }}>
-      {icon}
-      {eName}
-      {dName}
+function EventDetails({ icon, eName, dName }) {
+  return (
+    <div className='eventdetails' style={{
+      padding: '10px',
+      marginTop: '8px',
+      display: 'flex',
+      alignItems: 'center',
+      backgroundColor: 'white',
+      borderRadius: '12px'
+    }}>
+      <div style={{
+        padding: '5px',
+        marginRight: '15px',
+        marginLeft: '10px',
+        paddingLeft: '8px',
+        paddingRight: '8px',
+        borderRadius: '10px',
+        backgroundColor: '#E6DEFF',
+        color: '#BF83FF'
+      }}>
+        {icon}
+      </div>
+      <div className='' style={{
+        display: 'flex',
+        flexDirection: 'column',
+        color: 'black'
+      }}>
+        <span style={{ fontSize:'15px'}}>{eName}</span>
+        <span style={{ fontSize: '12px', color: '#B9B9B9' }}>{dName}</span>
+      </div>
     </div>
   )
 }
